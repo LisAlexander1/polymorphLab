7d88b98e0c2c873b6ed5a08f44bf7b5951d11ab5
lis <lis@LAPTOP-RAN47HJS>
2022-12-14 22:18:50 +0200

    Add Array steelList and calc fullArea and fullWeidth

--- Program.cs
+++ Program.cs
@@ -10,25 +10,47 @@ namespace polymorphLab
 
             do
             {
-                Steel square = new(random.Next(7600, 8500),
-                                   random.Next(1, 11),
-                                   random.NextDouble()*9500+500);
+                Steel[] steelList = new Steel[15];
+                for (int i = 0; i < steelList.Length; i++)
+                {
+                    
 
-                Steel tri = new TriangleList(random.Next(7600, 8500),
+                    switch (i)
+                    {
+                        case < 5:
+                            steelList[i] = new(random.Next(7600, 8500),
+                                   random.Next(1, 11),
+                                   random.NextDouble() * 9500 + 500);
+                            break;
+                        case < 12:
+                            steelList[i] = new RectangleList(random.Next(7600, 8500),
                                              random.Next(1, 11),
                                              random.NextDouble() * 9500 + 500,
                                              random.NextDouble() * 9500 + 500);
-
-                Steel rect = new RectangleList(random.Next(7600, 8500),
+                            break ;
+                        case < 16:
+                            steelList[i] = new TriangleList(random.Next(7600, 8500),
                                                random.Next(1, 11),
                                                random.NextDouble() * 9500 + 500,
                                                random.NextDouble() * 9500 + 500);
-
-                Console.WriteLine(square.Info());
-                Console.WriteLine(tri.Info());
-                Console.WriteLine(rect.Info());
+                            break;
+                    }
+                }
+                double fullArea = 0;
+                double fullWeidth = 0;
+                for (int i = 0; i < steelList.Length; i++)
+                {
+                    
+                    ConsoleColor color = i % 2 == 0 ? ConsoleColor.White : ConsoleColor.DarkCyan;
+                    Console.ForegroundColor = color;
+                    Console.WriteLine(steelList[i].Info());
+                    fullArea += steelList[i].GetArea();
+                    fullWeidth += steelList[i].GetWeight();
+                }
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"Суммарный вес: {fullWeidth:F1} кг, общая площадь: {fullArea:F2} м2");
                 Console.ForegroundColor = ConsoleColor.DarkGray;
-                Console.WriteLine("Нажмите любую кнопку для генерафции новых значений");
+                Console.WriteLine("Нажмите любую кнопку для генерации новых значений");
                 Console.ReadKey();
                 Console.Clear();
                 Console.ResetColor();
